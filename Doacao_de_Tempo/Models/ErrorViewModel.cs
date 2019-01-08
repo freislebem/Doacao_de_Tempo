1058e9bab3582703288aa7ef3466d6e90c7fefa6
Denis Freislebem <si.denis@gmail.com>
2019-01-08 14:04:08 -0200

    Submit initial

---
+++ Doacao_de_Tempo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Doacao_de_Tempo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
